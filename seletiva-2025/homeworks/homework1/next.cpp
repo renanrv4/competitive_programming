bb89ccf7a4d0c62c748f567d3ad9be837ac26e65
renanrv4 <renan.santana.arv@gmail.com>
2025-04-09 10:42:57 -0300

    AtCoder 329

--- seletiva-2025/homeworks/homework1/next.cpp
+++ seletiva-2025/homeworks/homework1/next.cpp
@@ -10,6 +10,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         int num; cin >> num;
         if(num > max1) {
+            max2 = max1;
             max1 = num;
         } else if(num > max2 && num < max1) {
             max2 = num;
