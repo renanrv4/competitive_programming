f73eef69a798f03b0087aac3593648931ffccf38
renanrv4 <renan.santana.arv@gmail.com>
2025-04-09 10:40:37 -0300

    AtCoder 329

---
+++ seletiva-2025/homeworks/homework1/next.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// AtCoder - abc329_b
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    int max1 = 0; int max2 = 0;
+    for(int i = 0; i < n; i++) {
+        int num; cin >> num;
+        if(num > max1) {
+            max1 = num;
+        } else if(num > max2 && num < max1) {
+            max2 = num;
+        }
+    }
+    cout << max2 << "\n";
+    return 0;
+}
